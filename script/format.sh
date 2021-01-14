230cddf621983ac1f2f0bdfa4fa3a9fa4491bdc2
Commelina <maosics@gmail.com>
2021-01-14 16:13:31 +0800

    add hstream-sql package

--- script/format.sh
+++ script/format.sh
@@ -2,5 +2,10 @@
 
 FORMATER_BIN=${FORMATER_BIN:-stylish-haskell}
 
-find . -type f -not -path "*/dist-newstyle/*" -not -path "*/.stack-work/*" | \
-    grep "\.l\?hs$" | xargs $FORMATER_BIN -c .stylish-haskell.yaml -i
+find . -type f -not -path "*/dist-newstyle/*" -not -path "*/.stack-work/*" \
+     -not -path */hstream-sql/src/Language/SQL/Abs.hs                      \
+     -not -path */hstream-sql/src/Language/SQL/Lex.hs                      \
+     -not -path */hstream-sql/src/Language/SQL/Par.hs                      \
+     -not -path */hstream-sql/src/Language/SQL/Print.hs                    \
+     -not -path */hstream-sql/src/Language/SQL/ErrM.hs                     \
+  | grep "\.l\?hs$" | xargs $FORMATER_BIN -c .stylish-haskell.yaml -i
